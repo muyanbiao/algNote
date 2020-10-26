d0affab958ad272a464ec6b1834d657d2793f489
yanbiaomu <yanbiaomu@tencent.com>
2020-10-27 07:52:34 +0800

    备份算法笔记代码

---
+++ LinkListDemo/LinkListDemo/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  LinkListDemo
+//
+//  Created by yanbiaomu on 2020/10/27.
+//
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+struct node { // 链表节点
+    int data;
+    node* next;
+};
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
