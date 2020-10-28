6feeb930e0ad1167b80406d124af25818ba3ac30
yanbiaomu <yanbiaomu@tencent.com>
2020-10-28 09:40:41 +0800

    feature：创建链表

--- LinkListDemo/LinkListDemo/main.cpp
+++ LinkListDemo/LinkListDemo/main.cpp
@@ -14,8 +14,37 @@ struct node { // 链表节点
     node* next;
 };
 
+node *createLinkList(int array[], int arrCnt) {
+    node *head;
+    head = new node;
+    head->next = NULL;
+    
+    node *pre;
+    pre = head;
+    
+    node *newP;
+    
+    for (int i = 0; i < arrCnt; ++i) {
+        newP = new node;
+        newP->data = array[i];
+        newP->next = NULL;
+        pre->next = newP;
+        pre = newP;
+    }
+    
+    return head;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    cout << "Hello, World!\n";
+    
+    int array[6] = {6, 5, 4, 3, 2, 1};
+    node *linkList = createLinkList(array, 6);
+    linkList = linkList->next;
+    while (linkList != NULL) {
+        cout << linkList->data << " ";
+        linkList = linkList->next;
+    }
+    
+    cout << endl;
     return 0;
 }
